e092f2aa700ec0e293cbefbaaa68de19736a9a0d
ibrahim ozocak <i.ozocak45@gmail.com>
2024-03-24 00:50:01 +0100

    organisation arbre, views, app.js, services

---
+++ Js/pages/Error404.js
@@ -0,0 +1,7 @@
+export default class Error404 {
+    async render() {
+        return `
+            <h1>Error 404: Page not found</h1>
+        `;
+    }
+}
---
+++ Js/pages/Home.js
@@ -0,0 +1,28 @@
+import PersonnageProvider from "../Services/PersonnageProvider.js";
+
+export default class Home {
+    async render() {
+        let personnages = await PersonnageProvider.fetchPersonnages(3);
+        return `
+            <h1>Les Personnages</h1>
+            <link rel="stylesheet" href="../../Css/home.css">
+            <div id="liste-perso">
+                ${personnages.map(personnage => `
+                    <div class="personnage">
+                        <div class="personnage-nom">
+                            <p>${personnage.nom}</p>
+                        </div>
+                        <div class="personnage-description">
+                            ${personnage.description}
+                            <p class="lien"><a href="/#/personnage/${personnage.id}">Voir plus</a></p>
+                        </div>
+                        <div class="personnage-anime">
+                            <p>${personnage.anime}</p>
+                        </div>
+                    </div>
+                    `
+                    ).join('\n')}
+            </div>
+        `;
+    }
+}
